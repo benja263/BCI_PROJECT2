5b7e13a751477eb0f6d9c0de4b177252218c41eb
Juraj Korcek <korcek.juraj@gmail.com>
2018-05-17 11:17:30 +0200

    classifier improved

--- classifier.m
+++ classifier.m
@@ -2,13 +2,11 @@
 addpath(genpath('./biosig')) %adds folder recursively
 addpath(genpath('./eeglab_current')) %adds folder recursively
 addpath(genpath('./functions')) %adds folder recursively
-load psd_data.mat
+% load psd_data.mat
 %% run
 tic
 name = 'emily';
 date = '20180326';
-name = 'benjamin';
-date = '20180319';
 
 freq = 4:2:48;
 data = day_selector(psd_file,name,date);
@@ -17,7 +15,7 @@ type = 1; % 1 car, 2 lap
 Classifier_plotFrequencyMap(data_for_train,type,name,date);
 fisher = calculateClassifierFisher(data_for_train,type)';
 %fisher = fisher./max(fisher(:));
-tol = 0.9*max(fisher(:));
+tol = 0.95*max(fisher(:));
 [channels,freq_ind] = find(fisher >= tol);
 psd_data = data_for_train{2,type};
 tr_events = data_for_train{2,3};
@@ -35,20 +33,9 @@ end
 n = size(tr_data,1);
 
 Model = fitcdiscr(tr_data,tr_events,'DiscrimType','linear', 'ClassNames',...
-    {'773','771'},'Prior','uniform');
-test_labels = data_for_test{2,3};
-post_prob = zeros(2,size(te_data,1));
-correct = zeros(1,size(te_data,1));
-for i = 1:size(te_data,1)
-    test_window = te_data(i,:);
-    [label,score,cost] = predict(Model,test_window);
-    post_prob(1,i) = score(1); % the classes are inversed in the model
-    post_prob(2,i) = score(2);
-    if strcmp(label{1},num2str(te_events(i)))
-        correct(i) = 1;
-    end
-end
-accuracy = sum(correct)/length(correct);
+    [773,771],'Prior','uniform');
+[label,post_prob,cost] = predict(Model,te_data);
+accuracy = sum(label == te_events) / length(te_events)
 
 toc
 
